f724b26432494e191f11eda3a50b532df00106f4
Rachael Korinek <rachael.korinek@gmail.com>
2018-11-13 11:27:55 -0600

    Add script to retrieve doc properties

---
+++ document_properties.rb
@@ -0,0 +1,36 @@
+# document_properties.rb - Return your Document Properties from the Report Content 
+# page of your Dradis project
+#
+# Copyright (C) 2018 Security Roots Ltd.
+#
+# This file is part of the Dradis Pro Scripting Examples (DPSE) collection.
+# The collection can be found at
+#   https://github.com/securityroots/dradispro-scripting
+#
+# DPSE free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# DPSE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DPSE.  If not, see <http://www.gnu.org/licenses/>.
+
+if ARGV.size != 1
+ puts "Usage:\n\tRAILS_ENV=#{Rails.env} bundle exec rails runner #{$0} <project_id>"
+ exit 1
+end
+
+pid = ARGV[0]
+
+Node.where(label: "Report content").each do |n|
+	prop_list = n.properties
+	puts "#{prop_list}"
+	prop_list.each_pair do |key, value|
+		puts "#{value}"
+	end
+end
