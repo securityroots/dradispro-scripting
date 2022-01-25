ce9046f9215bc09b24075c5dace320db23f98d7a
Michael Gargiullo <mgargiullo@users.noreply.github.com>
2022-01-25 10:02:50 -0500

    Update export-rules-for-kit.rb
    Boilerplate text added.
    
    Co-authored-by: sean-yeoh <sean.yeoh@hotmail.com>

--- export-rules-for-kit.rb
+++ export-rules-for-kit.rb
@@ -1,6 +1,25 @@
 
-# This script for Dradis will export the rules in a format that can be included in a Kit or
-# executed to build the ruleset
+# export_rules_for_kit.rb - Export the rules in a format that can be included in a Kit or
+#   executed to build the ruleset
+#
+# Copyright (C) 2017 Security Roots Ltd.
+#
+# This file is part of the Dradis Pro Scripting Examples (DPSE) collection.
+# The collection can be found at
+#   https://github.com/securityroots/dradispro-scripting
+#
+# DPSE free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# DPSE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DPSE.  If not, see <http://www.gnu.org/licenses/>.
 
 Dradis::Pro::Rules::Rule.find do |the_rule|
   puts "\# #{the_rule.name}"
