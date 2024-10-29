3a99128d02dbc7e1e18e5ec1d1671e6dac4097db
Rachael Carder <rachkor@users.noreply.github.com>
2024-10-29 16:09:07 -0500

    Update IssueLibrary entry state

--- update_issuelib_entries.rb
+++ update_issuelib_entries.rb
@@ -1,7 +1,7 @@
-# update_entries.rb - Updates all IssueLibrary entries to replace plugin_id with nessus_id (if found)
-#   and adds a new Status field to all entries. 
+# update_entries.rb - Updates all IssueLibrary entries to replace plugin_id with nessus_id (if found), 
+#   adds a new Status field, and sets the state to "Ready for Review"
 #
-# Copyright (C) 2017 Security Roots Ltd.
+# Copyright (C) 2024 Security Roots Ltd.
 #
 # This file is part of the Dradis Pro Scripting Examples (DPSE) collection.
 # The collection can be found at
@@ -23,10 +23,14 @@
 Dradis::Pro::Plugins::Issuelib::Entry.all.each do |libentry|
   libentry.content << "\r\n\r\n#[Status]#\r\nOpen | Closed\r\n"
   puts "Adding fields to IssueLibrary entry #{libentry.id}"
-  libentry.save
+  
   if libentry.content.include? "#[plugin_id]#"
     libentry.content.sub!('#[plugin_id]#', '#[nessus_id]#')
     libentry.save
     puts "Replacing fields in IssueLibrary entry #{libentry.id}"
   end
+
+  libentry.state = "ready_for_review"
+  puts "Setting entry #{libentry.id}'s state to 'Ready for Review'"
+  libentry.save
 end
