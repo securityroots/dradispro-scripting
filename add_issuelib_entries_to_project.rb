87fa1ae48e11e7c8513b741ef137f782952ff2bb
Rachael Carder <rachael.korinek@gmail.com>
2024-10-30 11:27:43 -0500

    Add script to push IssueLibrary entries to a project

---
+++ add_issuelib_entries_to_project.rb
@@ -0,0 +1,33 @@
+# add_issuelib_entries_to_project.rb - Adds all IssueLibrary entries to a specific project as Issues
+#
+# Copyright (C) 2024 Security Roots Ltd.
+#
+# This file is part of the Dradis Pro Scripting Examples (DPSE) collection.
+# The collection can be found at
+#   https://github.com/securityroots/dradispro-scripting
+#
+# DPSE free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# DPSE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DPSE.  If not, see <http://www.gnu.org/licenses/>.
+
+if ARGV.size != 1
+ puts "Usage:\n\tRAILS_ENV=#{Rails.env} bundle exec rails runner #{$0} <project_id>"
+ exit 1
+end
+
+pid = ARGV[0]
+project  = Project.find(pid)
+
+Dradis::Pro::Plugins::Issuelib::Entry.all.each do |entry|
+  issue = project.issues.create(content: entry.content)
+  issue.entry = entry
+end
